91a9f2b37fb35744c70004a717cc6c09e6fc0824
Karem <karem.huacota@gmail.com>
2018-10-26 12:07:36 -0400

    se modifico el modelo de empleado y el controller

--- Empleate/Models/Empleado.cs
+++ Empleate/Models/Empleado.cs
@@ -1,20 +1,45 @@
-﻿
-using System.Collections.Generic;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Empleate.Models
 {
     public class Empleado
     {
+        [Key]
+        [Column(Order = 1)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
+        [Required]
         public string Nombre { get; set; }
 
+        [Required]
         public string Apellidos { get; set; }
 
-        public int Telefono { get; set; }
+        [Required]
+        public DateTime FechaNacimiento { get; set; }
 
+        [Required]
+        public string Genero { get; set; }
+
+        [Required]
+        public string EstadoCivil { get; set; }
+
+        [Required]
+        public int Celular { get; set; }
+
+        [Required]
+        public int Ciudad { get; set; }
+
+        [Required]
+        public int Direccion { get; set; }
+
+        [Required]
         public string Correo { get; set; }
+
+        [Required]
+        public string Imagen { get; set; }
 
         public int ProfesionId { get; set; }
         [ForeignKey("ProfesionId")]
