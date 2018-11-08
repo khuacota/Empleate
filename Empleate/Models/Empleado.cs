190c9b3d1a02afabde0db150a13c4d544bab7e5c
Karem <karem.huacota@gmail.com>
2018-11-08 11:14:59 -0400

    Registro empleado finished

--- Empleate/Models/Empleado.cs
+++ Empleate/Models/Empleado.cs
@@ -12,40 +12,40 @@ namespace Empleate.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
-        [Required]
-        [RegularExpression(RegularExpression.TextValidation, ErrorMessage = "Nombre solo debe ser nombre")]
+        [Required(ErrorMessage = "Nombre es requerido")]
+        [RegularExpression(RegularExpression.TextValidation, ErrorMessage = "Nombre debe ser texto")]
         public string Nombre { get; set; }
 
-        [Required]
-        [RegularExpression(RegularExpression.TextValidation, ErrorMessage = "Apellidos debe ser solo texto")]
+        [Required(ErrorMessage = "Apellidos es requerido")]
+        [RegularExpression(RegularExpression.TextValidation, ErrorMessage = "Apellidos debe ser texto")]
         public string Apellidos { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Fecha de nacimiento es requerido")]
         [AgeCheck]
         [DataType(DataType.Date)]
         public DateTime FechaNacimiento { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Género es requerido")]
         public string Genero { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Estado civil es requerido")]
         public string EstadoCivil { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Celular es requerido")]
         [RegularExpression(RegularExpression.CellphoneValidation, ErrorMessage = "Numero de celular debe empezar con 6 o 7")]
         public int Celular { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Ciudad es requerido")]
         public string Ciudad { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Dirección es requerido")]
         public string Direccion { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Correo es requerido")]
         [EmailAddress]
         public string Correo { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Imagen es requerido")]
         [RegularExpression(RegularExpression.ImageValidation, ErrorMessage = "Formato de imagen invalido")]
         public string Imagen { get; set; }
     }
